21f2eb95cc7837e7177309a62688ef9fa8d64162
David Oluwafemi <davidoluwafemi178@gmail.com>
2021-11-24 21:22:17 +0100

    add routes

--- node-project/server1.js
+++ node-project/server1.js
@@ -1,17 +1,23 @@
 const http = require('http')
 const fs = require('fs').promises
+
+let indexFile;
+
 const requestListener = function (req, res) {
-    fs.readFile(__dirname + "/index.html")
-        .then(contents => {
-          //res.setHeader("Content-Type", "text/html");
-          res.writeHead(200, {"Content-Type": "text/html"});
-          res.end(contents);
-        })
-        .catch(err => {
-            res.writeHead(500);
-            res.end(err);
-            return;
-        });
+  res.writeHead(200, {"Content-Type": "text/html"});
+  res.end(indexFile);
 };
+
 const server = http.createServer(requestListener)
-server.listen(3000, console.log("Server running..."));
+
+fs.readFile(__dirname + "/index.html")
+  .then(contents => {
+    indexFile = contents
+    server.listen(3000, console.log("Server running..."));
+  })
+  .catch(err => {
+    res.writeHead(500);
+    console.log(err)
+    res.end(err);
+     return;
+    });
