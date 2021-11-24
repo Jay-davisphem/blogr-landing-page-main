26bc1128360fbdd20862cda5dbe930ace62cb383
David Oluwafemi <davidoluwafemi178@gmail.com>
2021-11-24 20:47:59 +0100

    add server project1

---
+++ node-project/server1.js
@@ -0,0 +1,17 @@
+const http = require('http')
+const fs = require('fs').promises
+const requestListener = function (req, res) {
+    fs.readFile(__dirname + "/index.html")
+        .then(contents => {
+          //res.setHeader("Content-Type", "text/html");
+          res.writeHead(200, {"Content-Type": "text/html"});
+          res.end(contents);
+        })
+        .catch(err => {
+            res.writeHead(500);
+            res.end(err);
+            return;
+        });
+};
+const server = http.createServer(requestListener)
+server.listen(3000, console.log("Server running..."));
